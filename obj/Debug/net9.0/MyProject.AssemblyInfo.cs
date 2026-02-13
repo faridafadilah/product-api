9580274f96ffca9ec2ecb9231aa6669457d48fa9
Farida Fadilah <farida.f@kiranatama.com>
2026-02-13 13:03:38 +0700

    add readme

--- obj/Debug/net9.0/MyProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1df2e459c7f8cefb675c27d2a50f6fe9e2a9fa3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
